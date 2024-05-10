23e9d9d4cc5cdfd6c5f225c5a7acd747ec34fca8
Andrea01-debug <andymito01@gmail.com>
2024-05-11 00:18:59 +0200

    tavolirossifatti

--- ImageMapster-main/site/src/components/features/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as FeatureDetail } from './feature-detail.astro';
-export { default as FeatureGrid } from './feature-grid.astro';
-export { default as Feature } from './feature.astro';
